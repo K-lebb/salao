a656f45ac36ebb7bfd07058ce5052264714bfaa3
Kaleb <gabrielkalebaragao@gmail.com>
2024-11-21 15:32:57 -0300

    first commit

---
+++ cads.js
@@ -0,0 +1,10 @@
+var log = document.getElementsByName('log');
+var cad = document.getElementsByName('cad');
+
+function consultar(){
+    console.log('olha');
+}
+
+function cadastrar(){
+    console.log('olha');
+}
